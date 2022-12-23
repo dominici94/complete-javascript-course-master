6ec711085df4affed248be8b0b45e7788ff22685
dominici94 <mattiadominici94@gmail.com>
2022-12-23 18:22:06 +0100

    oop start

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // BAD PRACTICE mai creare un metodo dentro un costruttore
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+// class Person {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
+// }
+
+const jonas = new Person('Jonas', 1991);
+
+// 1. New {} is created (object)
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
