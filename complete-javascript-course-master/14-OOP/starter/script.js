8a6cafacbb67bc8d0d9e52eccefca31868a50bef
dominici94 <mattiadominici94@gmail.com>
2022-12-27 17:51:47 +0100

    es. 1 oop

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -31,3 +31,47 @@ const jonas = new Person('Jonas', 1991);
 
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
+
+// Coding Challenge #1
+// Your tasks:
+// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+// 'speed' property. The 'speed' property is the current speed of the car in
+// km/h
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10,
+// and log the new speed to the console
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+// the new speed to the console
+// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+// 'brake' multiple times on each of them
+// Test data:
+// § Data car 1: 'BMW' going at 120 km/h
+// § Data car 2: 'Mercedes' going at 95 km/h
+// GOOD LUCK 😀
+
+// 1.
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+// 2.
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+};
+// 3.
+Car.prototype.brake = function () {
+  this.speed -= 5;
+};
+
+// 4.
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+console.log(bmw);
+console.log(mercedes);
+
+bmw.accelerate();
+debugger;
+bmw.accelerate();
+debugger;
+mercedes.brake();
