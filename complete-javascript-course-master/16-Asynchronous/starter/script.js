76dda567ae69e0e84ee3bcd1d517d35b8def930a
dominici94 <mattiadominici94@gmail.com>
2023-01-16 17:12:32 +0100

    start API call

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,47 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+      <article class="country">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1_000_000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${
+            data.languages.por ||
+            data.languages.eng ||
+            data.languages.ita ||
+            data.languages.deu
+          }</p>
+          <p class="country__row"><span>💰</span>${
+            data.currencies.EUR?.name
+              ? data.currencies.EUR.name
+              : data.currencies.USD.name
+          }</p>
+          </div>
+          </article>
+          `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('italy');
+getCountryData('germany');
