1bfd78bec3245e2c5d4516cc10f61ac05e8a8052
dominici94 <mattiadominici94@gmail.com>
2023-01-30 19:20:13 +0100

    finish course app

--- complete-javascript-course-master/18-forkify/starter/src/js/config.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '8dbc9d3d-cb4f-42d2-ba2c-67c5497eed1f';
+export const MODAL_CLOSE_SEC = 2.5;
