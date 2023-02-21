26895df6ac451b459b0fb05517c4f76f989ae7d7
dominici94 <mattiadominici94@gmail.com>
2023-02-21 19:25:49 +0100

    fix pig game

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -8,91 +8,110 @@ const scoreTotal1 = document.getElementById('score--1');
 const current0 = document.getElementById('current--0');
 const current1 = document.getElementById('current--1');
 const dice = document.querySelector('.dice');
-const newGame = document.querySelector('.btn--new')
+const newGame = document.querySelector('.btn--new');
 const roll = document.querySelector('.btn--roll');
 const hold = document.querySelector('.btn--hold');
 
-// condizioni di partenza
-scoreTotal0.textContent = 0;
-scoreTotal1.textContent = 0;
-dice.classList.add('hidden');
+// Funzioni
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+function rollDice() {
+  return Math.round(Math.random() * 5) + 1;
+}
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+}
+
+// condizioni di partenza
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  scoreTotal0.textContent = 0;
+  scoreTotal1.textContent = 0;
+  current0.textContent = 0;
+  current1.textContent = 0;
+
+  dice.classList.add('hidden');
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+};
+init();
 
 // AL CLICK SU ROLL DICE
 roll.addEventListener('click', function () {
-    if (playing) {
-        // genero un dado casuale
-        let casual = rollDice();
+  if (playing) {
+    // genero un dado casuale
+    let casual = rollDice();
 
-        // mostro il dado a schermo
-        dice.classList.remove('hidden');
-        dice.src = `dice-${casual}.png`;
+    // mostro il dado a schermo
+    dice.classList.remove('hidden');
+    dice.src = `dice-${casual}.png`;
 
-        // aggiungo il dado al punteggio corrente se diverso da 1
-        if (casual !== 1) {
-            currentScore += casual;
-            // selezione dinamica del giocatore
-            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-        } else {
-            // passo al prossimo giocatore
-            switchPlayer();
-        }
-
+    // aggiungo il dado al punteggio corrente se diverso da 1
+    if (casual !== 1) {
+      currentScore += casual;
+      // selezione dinamica del giocatore
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // passo al prossimo giocatore
+      switchPlayer();
     }
+  }
 });
 
-// AL CLICK SUU HOLD
+// AL CLICK SU HOLD
 hold.addEventListener('click', function () {
-    if (playing) {
-        // AGGIUNGO IL PIUNTEGGIO CORRENTE AL PUNTEGGIO TOTALE DEL GIOCATORE ATTIVO
-        scores[activePlayer] += currentScore;
-        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+  if (playing) {
+    // AGGIUNGO IL PIUNTEGGIO CORRENTE AL PUNTEGGIO TOTALE DEL GIOCATORE ATTIVO
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-
-        // se un giocatore supera i 99 punti vince
-        if (scores[activePlayer] >= 10) {
-            playing = false;
-            dice.classList.add('hidden');
-            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-            document.querySelector(`.player--${activePlayer}`).classList.remove('active--player');
-        } else {
-            switchPlayer();
-        }
+    // se un giocatore supera i 99 punti vince
+    if (scores[activePlayer] >= 99) {
+      playing = false;
+      dice.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('active--player');
+    } else {
+      switchPlayer();
     }
+  }
 });
 
 // AL CLICK SU NEW GAME
 
 newGame.addEventListener('click', function () {
-    playing = true;
-    scores[0] = 0;
-    scores[1] = 0;
-    currentScore = 0;
-    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
-    document.querySelector(`.player--${activePlayer}`).classList.add('active--player');
-    scoreTotal0.textContent = 0;
-    scoreTotal1.textContent = 0;
-    current0.textContent = 0;
-    current1.textContent = 0;
-    dice.classList.add('hidden');
-
+  playing = true;
+  scores[0] = 0;
+  scores[1] = 0;
+  currentScore = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('active--player');
+  scoreTotal0.textContent = 0;
+  scoreTotal1.textContent = 0;
+  current0.textContent = 0;
+  current1.textContent = 0;
+  dice.classList.add('hidden');
 });
-
-
-// FUNZIONI
-
-function rollDice() {
-    return (Math.round(Math.random() * 5) + 1);
-}
-
-function switchPlayer() {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0.classList.toggle('player--active');
-    player1.classList.toggle('player--active');
-}
